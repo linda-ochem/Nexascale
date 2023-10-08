bdc4ab876cad3215d9b78d99a3783aef59b992de
linda-ochemm <lindaochem@gmail.com>
2023-10-08 15:02:48 +0100

    azure test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8081;
 
 app.get('/', (req, res) => {
-  res.send('This you simple Node.js server written with my pc!');
+  res.send('simple Node.js server written with my pc!');
 });
 
 app.listen(PORT, () => {
